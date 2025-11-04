1b5c083ea92103e29fa7acd91143dff478709fed
Ajeet Kumar <ajeetkumr567@gmail.com>
2025-11-04 07:47:38 +0630

    Initial commit

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'safety',
+    'purchase_key' => 'safety',
+    'software_id' => 'MzE0NDg1OTc=',
+    'domain' => '127.0.0.1:8000',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'username' => 'safety',
+    'purchase_key' => 'safety',
+    'software_id' => 'MzYwODUwNDE=',
+    'domain' => '127.0.0.1:8000',
+    'software_type' => 'addon',
+  ),
+  'theme_lifestyle' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
